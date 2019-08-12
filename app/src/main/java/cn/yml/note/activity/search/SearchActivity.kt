1b48a96090a93092f222c8a1d80c1ddbe9537046
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-08-12 16:01:19 +0800

    fix: 修复搜索结果不能点击查看详情的bug

--- app/src/main/java/cn/yml/note/activity/search/SearchActivity.kt
+++ app/src/main/java/cn/yml/note/activity/search/SearchActivity.kt
@@ -6,19 +6,30 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.recyclerview.widget.LinearLayoutManager
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.UpdateListener
+import cn.yml.note.App
 import cn.yml.note.R
+import cn.yml.note.activity.edit.EditActivity
 import cn.yml.note.activity.main.NoteAdapter
+import cn.yml.note.extensions.jumpTo
 import cn.yml.note.extensions.toJson
 import cn.yml.note.extensions.toast
 import cn.yml.note.model.Image
 import cn.yml.note.model.Note
+import cn.yml.note.model.delete
+import cn.yml.note.model.params.IntentParam
 import cn.yml.note.utils.GsonUtil
 import cn.yml.note.utils.database
 import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.activity_search.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.appcompat.v7.Appcompat
 import org.jetbrains.anko.db.parseList
 import org.jetbrains.anko.db.rowParser
 import org.jetbrains.anko.db.select
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
 class SearchActivity : AppCompatActivity() {
 
@@ -54,6 +65,41 @@ class SearchActivity : AppCompatActivity() {
                     }
                 }
         }
+
+        // 点击便签Item进入便签预览界面
+        noteAdapter?.setOnItemClickListener { adapter, view, position ->
+            App.note = noteAdapter!!.getItem(position)
+            jumpTo(
+                EditActivity::class.java, IntentParam()
+                    .add(EditActivity.PARAM_MODE, 1)
+            )
+        }
+        noteAdapter?.onBlankAreaClick = object : NoteAdapter.OnBlankAreaClickListener {
+            override fun onItemClick(adapter: NoteAdapter, item: Note) {
+                App.note = item
+                jumpTo(
+                    EditActivity::class.java, IntentParam()
+                        .add(EditActivity.PARAM_MODE, 1)
+                )
+            }
+        }
+
+        // 长按删除
+        noteAdapter?.setOnItemLongClickListener { adapter, view, position ->
+            val item = noteAdapter!!.getItem(position)
+            alert(Appcompat, "", "确认要删除当前便签？") {
+                yesButton {
+                    item?.delete(object : UpdateListener() {
+                        override fun done(p0: BmobException?) {
+                            noteAdapter?.remove(position)
+                        }
+                    }, this@SearchActivity)
+                }
+                noButton {}
+            }.show()
+            return@setOnItemLongClickListener true
+        }
+
     }
 
 
